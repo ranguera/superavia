0ec6887f6dc4324e0173ae11ee4163a9b0971246
Roger Anguera <ranguera@gmail.com>
2019-01-08 12:03:35 -0800

    init

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float sp1 = 1f;
+    public float sp2 = 2f;
+    public float sp3 = 3f;
+
+    public GameObject moving1;
+    public GameObject moving2;
+    public GameObject moving3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.moving1.transform.Translate(Vector3.left * sp1 * Time.deltaTime);
+        this.moving2.transform.Translate(Vector3.left * sp2 * Time.deltaTime);
+        this.moving3.transform.Translate(Vector3.left * sp3 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Go());
+        }
+    }
+
+    private IEnumerator Go()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Game");
+    }
+}
